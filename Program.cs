dce4b7bc32fe7601cff2c991d8d0ddb418798300
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-03 12:13:42 +0200

    created control flow of the program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using zipcrack.Enums;
+
+namespace zipcrack;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            ConsoleLogger.LogUsage();
+            return;
+        }
+
+        string? password = null;
+        AttackContext context;
+        var attackMethod = ArgumentParser.ParseAttackMethod(args[0]);
+
+        if (attackMethod == AttackMethod.Dictionary)
+        {
+            context = new AttackContext(new DictionaryAttack(new DictionaryArgumentParser(args)));
+            password = context.Execute();
+        }
+
+        if (attackMethod == AttackMethod.BruteForce)
+        {
+            context = new AttackContext(new BruteForceAttack(new BruteForceArgumentParser(args)));
+            password = context.Execute();
+        }
+
+        if (password == null)
+        {
+            ConsoleLogger.LogPasswordNotFound();
+            
+        }
+        else
+        {
+            ConsoleLogger.LogPasswordFound(password);
+        }
+    }
+}
