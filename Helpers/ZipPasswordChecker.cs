9573dd5cc015bc10633ce7f80da488ab7b3671d8
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-03 12:36:15 +0200

    class to check if password to extract zip file is valid

---
+++ Helpers/ZipPasswordChecker.cs
@@ -0,0 +1,40 @@
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace zipcrack.Helpers;
+
+public class ZipPasswordChecker
+{
+    private readonly string _zipFilePath;
+
+    public ZipPasswordChecker(string zipFilePath)
+    {
+        _zipFilePath = zipFilePath;
+    }
+    
+    public bool IsValid(string password)
+    {
+        try
+        {
+            using (ZipFile zip = new ZipFile(File.OpenRead(_zipFilePath)))
+            {
+                zip.Password = password;
+                foreach (ZipEntry entry in zip)
+                {
+                    if (!entry.IsFile) continue;
+
+                    using (Stream zipStream = zip.GetInputStream(entry))
+                    using (FileStream fs = File.Create(entry.Name))
+                    {
+                        zipStream.CopyTo(fs);
+                    }
+                }
+
+                return true;
+            }
+        }
+        catch (ZipException)
+        {
+            return false;
+        }
+    }
+}
