04bf591efb3a554f8ac144191dfc2a32b2853ff8
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-16 21:34:18 +0200

    added needed fields

--- AttackMethods/BruteForceAttack.cs
+++ AttackMethods/BruteForceAttack.cs
@@ -5,11 +5,17 @@ namespace zipcrack.AttackMethods;
 
 public class BruteForceAttack : IZipCracker
 {
-    private BruteForceArgumentParser _args;
+    private readonly int _minPasswordLength;
+    private readonly int _maxPasswordLength;
+    private readonly char[] _characters;
+    private readonly string _filePath;
     
     public BruteForceAttack(BruteForceArgumentParser argumentParser)
     {
-        _args = argumentParser;
+        _minPasswordLength = argumentParser.MinChars;
+        _maxPasswordLength = argumentParser.MaxChars;
+        _characters = argumentParser.Characters;
+        _filePath = argumentParser.FilePath;
     }
     
     public string? GetPassword()
