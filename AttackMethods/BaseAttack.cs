db4e9eb098b16d54c24fa906ed696bef76ab1fb6
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-17 15:20:14 +0200

    create abstract class

---
+++ AttackMethods/BaseAttack.cs
@@ -0,0 +1,20 @@
+using zipcrack.Helpers;
+using zipcrack.Interfaces;
+
+namespace zipcrack.AttackMethods;
+
+public abstract class BaseAttack : IZipCracker
+{
+    protected readonly string _zipFilePath;
+    protected readonly int _threadCount;
+    protected readonly ZipPasswordChecker _passwordChecker;
+
+    protected BaseAttack(string filePath)
+    {
+        _zipFilePath = filePath;
+        _threadCount = Environment.ProcessorCount;
+        _passwordChecker = new ZipPasswordChecker(filePath);
+    }
+
+    public abstract string? GetPassword();
+}
