67c45682eb997dd8345ba05a9ccf7400275a8555
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-31 13:35:47 +0300

    added password check to GenerateStringCombinations

--- AttackMethods/BruteForceAttack.cs
+++ AttackMethods/BruteForceAttack.cs
@@ -8,6 +8,7 @@ public class BruteForceAttack : BaseAttack
     private readonly int _minPasswordLength;
     private readonly int _maxPasswordLength;
     private readonly char[] _characters;
+    private readonly object _lock = new();
     
     public BruteForceAttack(BruteForceArgumentParser argumentParser) : base(argumentParser.FilePath, argumentParser.ThreadCount)
     {
@@ -24,19 +25,21 @@ public class BruteForceAttack : BaseAttack
 
     private void GenerateStringCombinations(string current = "")
     {
-        if (current.Length >= _minPasswordLength)
+        if (current.Length >= _minPasswordLength && _passwordChecker.IsValid(current))
         {
-            Console.WriteLine(current);
+            _password = current;
+            return;
         }
-
+        
         if (current.Length == _maxPasswordLength)
         {
             return;
         }
-
-
+        
         foreach (char c in _characters)
         {
+            if (_password != null) break;
+            
             GenerateStringCombinations(current + c);
         }
     }
