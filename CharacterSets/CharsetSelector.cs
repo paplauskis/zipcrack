92d44cf68d2f5add95f87124b28a916df01842e0
Kostas Paplauskas <paplauskaskostas@gmail.com>
2025-03-31 14:59:16 +0300

    removed Func delegate from Dictionary

--- CharacterSets/CharsetSelector.cs
+++ CharacterSets/CharsetSelector.cs
@@ -2,21 +2,21 @@ namespace zipcrack.CharacterSets;
 
 public static class CharsetSelector
 {
-    private static readonly Dictionary<string, Func<char[]>> CharSetSelectors = new()
+    private static readonly Dictionary<string, char[]> CharSetSelectors = new()
     {
-        { "l", () => PasswordCharacters.LowercaseLetters },
-        { "u", () => PasswordCharacters.UppercaseLetters },
-        { "n", () => PasswordCharacters.Numbers },
-        { "lu", () => PasswordCharacters.AllLetters },
-        { "lun", () => PasswordCharacters.AllLettersAndNumbers },
-        { "luns", () => PasswordCharacters.AllCharacters }
+        { "l", PasswordCharacters.LowercaseLetters },
+        { "u", PasswordCharacters.UppercaseLetters },
+        { "n", PasswordCharacters.Numbers },
+        { "lu", PasswordCharacters.AllLetters },
+        { "lun", PasswordCharacters.AllLettersAndNumbers },
+        { "luns", PasswordCharacters.AllCharacters }
     };
 
     public static char[] GetCharacterSet(string value)
     {
         if (CharSetSelectors.TryGetValue(value, out var selector))
         {
-            return selector();
+            return selector;
         }
 
         throw new ArgumentException("Argument is not valid", nameof(value));
